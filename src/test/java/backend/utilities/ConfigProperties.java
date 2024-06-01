f2e7cebc3d33d70fe11730caf822c071ec4e5fc7
Hector Vergara <hector-19-20@hotmail.com>
2024-05-31 21:54:01 -0500

    project uploaded

---
+++ src/test/java/backend/utilities/ConfigProperties.java
@@ -0,0 +1,37 @@
+package backend.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigProperties {
+    private static Properties properties;
+    private static final String PATH_PROPERTIES_FILE = "src/test/java/resources/configuration.properties";
+
+    public static String getProperty(String key) {
+        return readPropertiesFile().getProperty(key);
+    }
+
+    public static Properties readPropertiesFile() {
+        Properties properties = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream(PATH_PROPERTIES_FILE)) {
+            properties.load(fileInputStream);
+            return properties;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Properties writePropertiesFile(String key, String value) {
+        Properties properties =  readPropertiesFile();
+        properties.setProperty(key,value);
+        try (FileOutputStream  fileOutputStream = new FileOutputStream (PATH_PROPERTIES_FILE)) {
+            properties.store(fileOutputStream, null);
+            return properties;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/backend/utilities/pojo/Tag.java
@@ -0,0 +1,11 @@
+package backend.utilities.pojo;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Tag {
+    private int id;
+    private String name;
+}
